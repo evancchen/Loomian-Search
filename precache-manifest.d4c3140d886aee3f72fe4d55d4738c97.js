dab8c2521e6e8773750cdd396be42f8104bedc9a
evancchen <evancchen2009@gmail.com>
2020-07-15 10:48:51 -0700

    Updates

---
+++ precache-manifest.d4c3140d886aee3f72fe4d55d4738c97.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "694a410f2aa71d6e237002bb6a4ee4e0",
+    "url": "/Loomian-Search/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/Loomian-Search/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1c196fe2c12a96316624",
+    "url": "/Loomian-Search/static/css/main.4c514dc2.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/Loomian-Search/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Loomian-Search/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c196fe2c12a96316624",
+    "url": "/Loomian-Search/static/js/main.ba66434d.chunk.js"
+  },
+  {
+    "revision": "ebb08d50511cb2c1aae1",
+    "url": "/Loomian-Search/static/js/runtime-main.f28ab6d2.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Loomian-Search/static/media/SEGA.12a3db8b.woff"
+  }
+]);
