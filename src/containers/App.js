53b07a1cbb87cb504b6fbb1ca8758f3fe52c525f
evancchen <evancchen2009@gmail.com>
2020-07-22 10:58:43 -0700

    Update

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,30 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import {loomians} from '../loomians'; 
 import ErrorBoundry from '../components/ErrorBoundry'
 import Scroll from '../components/Scroll';
 import './App.css';
+import {setSearchField} from '../actions'
+
+const mapStateToProps = (state) => {
+    return {        
+        searchField: state.searchField
+    }
+}
+ 
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+}
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
             loomians: [],
-            searchfield: ''
         }
     }
     
@@ -19,19 +32,18 @@ class App extends Component {
         this.setState({loomians: loomians})
     }
 
-    onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value})
-    }
 
     render() {
-        const filteredLoomians = this.state.loomians.filter(loomian => loomian.name.toLowerCase().includes(this.state.searchfield.toLowerCase()));
+        const {loomians} = this.state;
+        const {searchField, onSearchChange} = this.props;
+        const filteredLoomians = loomians.filter(loomian => loomian.name.toLowerCase().includes(searchField.toLowerCase()));
         if (this.state.loomians.length === 0 ) {
             return <h1 className = 'tc'>Loading</h1>
         } else {
             return (
                 <div className = 'tc'>
                     <h1 className = "f1">Loomians</h1>
-                    <SearchBox searchChange = {this.onSearchChange}/>
+                    <SearchBox searchChange = {onSearchChange}/>
                     <Scroll>
                         <ErrorBoundry>
                             <CardList loomians = {filteredLoomians}/>
@@ -43,4 +55,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
